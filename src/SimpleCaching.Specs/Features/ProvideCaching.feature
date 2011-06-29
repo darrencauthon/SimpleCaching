2221bbfcb5729299f1cd7218597543fabb6d1d0d
Darren Cauthon <darren@cauthon.com>
2011-06-29 16:59:26 -0500

    Start working on the first spec.

---
+++ src/SimpleCaching.Specs/Features/ProvideCaching.feature
@@ -0,0 +1,10 @@
+﻿Feature: Provide caching
+	In order to make caching easy
+	As a programmer
+	I want to inherit from a cache object that handles caching in memory
+
+Scenario: Can call a method on the base product repository
+	Given I have an instance of the product repository class
+	And I have a cached product repository class
+	When I call the GetAll method on the product repository class
+	Then the base product repository class should be called once 
---
+++ src/SimpleCaching.Specs/Steps/ProductRepositorySteps.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using TechTalk.SpecFlow;
+
+namespace SimpleCaching.Specs.Steps
+{
+    [Binding]
+    public class ProductRepositorySteps
+    {
+        private Mock<IProductRepository> fakeProductRepository;
+
+        [Given(@"I have an instance of the product repository class")]
+        public void x()
+        {
+            fakeProductRepository = new Mock<IProductRepository>();
+        }
+    }
+
+    public interface IProductRepository
+    {
+    }
+}
